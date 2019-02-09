73c616713835e4380439ccb75cced1a41c42918d
Etienne Soulard-Geoffrion <esoulardgeoffrion@clinia.ca>
2019-02-09 15:15:24 -0500

    Modified store and app to use redux-saga/kea-saga

--- examples/with-kea-saga/pages/_app.js
+++ examples/with-kea-saga/pages/_app.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { compose } from 'redux'
 import App, { Container } from 'next/app'
 import withRedux from 'next-redux-wrapper'
-import { initStore } from '../store'
+import withReduxSaga from 'next-redux-saga'
+import { configureStore } from '../store'
 
 class MyApp extends App {
   static async getInitialProps ({ Component, ctx }) {
@@ -24,6 +26,7 @@ class MyApp extends App {
   }
 }
 
-export default withRedux(initStore, {
-  debug: process.env.NODE_ENV === 'development'
-})(MyApp)
+export default compose(
+  withRedux(configureStore, { debug: process.env.NODE_ENV === 'development' }),
+  withReduxSaga({ async: false })
+)(MyApp)
--- examples/with-kea-saga/store.js
+++ examples/with-kea-saga/store.js
@@ -1,15 +1,37 @@
-import { keaReducer } from 'kea'
-import { createStore, compose, combineReducers } from 'redux'
+import { activatePlugin, keaReducer } from 'kea'
+import sagaPlugin, { keaSaga } from 'kea-saga'
+import { applyMiddleware, compose, createStore, combineReducers } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 const reducers = combineReducers({
-  kea: keaReducer('kea')
+  kea: keaReducer('kea'),
+  scenes: keaReducer('scenes')
 })
 
-const reduxDevTools =
-  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__
-    ? window.__REDUX_DEVTOOLS_EXTENSION__()
-    : f => f
+const sagaMiddleware = createSagaMiddleware()
 
-export const initStore = () => {
-  return createStore(reducers, compose(reduxDevTools))
+const bindMiddleware = middleware => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension')
+    return composeWithDevTools(applyMiddleware(...middleware))
+  }
+
+  return applyMiddleware(...middleware)
+}
+
+export const configureStore = (initialState = {}) => {
+  activatePlugin(sagaPlugin)
+
+  const finalCreateStore = compose(bindMiddleware([sagaMiddleware]))(
+    createStore
+  )
+  const store = finalCreateStore(reducers, initialState)
+
+  store.runSagaTask = () => {
+    store.sagaTask = sagaMiddleware.run(keaSaga)
+  }
+
+  store.runSagaTask()
+
+  return store
 }
