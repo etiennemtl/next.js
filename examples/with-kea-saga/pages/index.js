cbe36596bf4d9ae7b1bb3a9f9c0119b61df9ffd3
Etienne Soulard-Geoffrion <esoulardgeoffrion@clinia.ca>
2019-02-09 15:47:06 -0500

    Modified logic & added load data saga

--- examples/with-kea-saga/pages/index.js
+++ examples/with-kea-saga/pages/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { kea } from 'kea'
+import { call, put } from 'redux-saga/effects'
+import axios from 'axios'
 
 class Homepage extends React.Component {
+  static async getInitialProps ({ store, isServer }) {
+    return { isServer }
+  }
+
+  componentDidMount () {
+    const { actions } = this.props
+    actions.loadData()
+  }
+
   render () {
     return (
       <div>
@@ -18,27 +29,70 @@ class Homepage extends React.Component {
   }
 }
 
+const exampleInitialState = {
+  count: 0,
+  error: false,
+  lastUpdate: 0,
+  light: false,
+  placeholderData: null
+}
+
 export default kea({
-  path: () => ['kea'],
+  path: () => ['scenes'],
   actions: () => ({
     increment: amount => ({ amount }),
-    decrement: amount => ({ amount })
+    decrement: amount => ({ amount }),
+    loadData: () => ({}),
+    loadDataSuccess: data => ({ data }),
+    loadDataFail: error => ({ error })
   }),
   reducers: ({ actions }) => ({
     counter: [
-      0,
-      PropTypes.number,
+      exampleInitialState,
+      PropTypes.object,
       {
-        [actions.increment]: (state, payload) => state + payload.amount,
-        [actions.decrement]: (state, payload) => state - payload.amount
+        [actions.increment]: (state, payload) => {
+          return {
+            ...state,
+            ...{ count: state.count + payload.amount }
+          }
+        },
+        [actions.decrement]: (state, payload) => {
+          return {
+            ...state,
+            ...{ count: state.count - payload.amount }
+          }
+        },
+        [actions.loadDataSuccess]: (state, payload) => {
+          return {
+            ...state,
+            ...{ placeholderData: payload.data }
+          }
+        }
       }
     ]
   }),
   selectors: ({ selectors }) => ({
     doubleCounter: [
       () => [selectors.counter],
-      counter => counter * 2,
+      counter => counter.count * 2,
       PropTypes.number
     ]
-  })
+  }),
+  takeEvery: ({ actions, workers }) => ({
+    [actions.loadData]: workers.loadDataSaga
+  }),
+  workers: {
+    loadDataSaga: function * (action) {
+      const { loadDataSuccess, loadDataFail } = this.actions
+      try {
+        const { data } = yield call(
+          axios.get('https://jsonplaceholder.typicode.com/users')
+        )
+        yield put(loadDataSuccess(data))
+      } catch (error) {
+        yield put(loadDataFail(error))
+      }
+    }
+  }
 })(Homepage)
